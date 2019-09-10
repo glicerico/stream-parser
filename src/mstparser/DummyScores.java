a8e7ca350e26346183ff3321f54b0ce3f3547b6b
glicerico <suarezandres@gmail.com>
2019-09-10 14:10:54 +0800

    modified runparser to use new vocab class

--- src/mstparser/DummyScores.java
+++ src/mstparser/DummyScores.java
@@ -17,12 +17,12 @@ public class DummyScores {
 	HashMap<String, Float> dummyScoreTable = new HashMap<String, Float>();
 
 	public DummyScores() {
-		this.dummyScoreTable.put("1_2", 1.0f);
-		this.dummyScoreTable.put("1_3", 1.1f);
-		this.dummyScoreTable.put("1_4", 1.2f);
-		this.dummyScoreTable.put("2_3", 0.5f);
-		this.dummyScoreTable.put("2_4", 0.6f);
-		this.dummyScoreTable.put("3_4", 0.8f);
+		this.dummyScoreTable.put("0_1", 1.0f);
+		this.dummyScoreTable.put("0_2", 1.1f);
+		this.dummyScoreTable.put("0_3", 1.2f);
+		this.dummyScoreTable.put("1_2", 0.5f);
+		this.dummyScoreTable.put("1_3", 0.6f);
+		this.dummyScoreTable.put("2_3", 0.8f);
 	}
       
     public HashMap returnScoreTable() {
