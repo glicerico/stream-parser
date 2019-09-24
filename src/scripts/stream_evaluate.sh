e4d21c6c8e0d6cdb469a302efb1ced00afd60b95
glicerico <suarezandres@gmail.com>
2019-09-24 23:16:26 +0800

    added window parameters as CL args

--- src/scripts/stream_evaluate.sh
+++ src/scripts/stream_evaluate.sh
@@ -5,16 +5,16 @@
 # Usage:  stream_evaluate.sh <vocabFilePath> <corpusPath> <parsesPath> <GSPath>
 
 STREAMPATH='/home/andres/IdeaProjects/stream-parser';
-winObserve=6;
-winParse=30;
+winObserve=$1;
+winParse=$2;
 calculateScores=true;
 exportScores=false;
-vocabFilePath=$1;
+vocabFilePath=$3;
 scoresFilePath="/home/andres/Documents/CAiN/trash.scores";
-observeCorpusPath=$2;
-corpusPath=$2;
-parsesPath=$3;
-GSPath=$4;
+observeCorpusPath=$4;
+corpusPath=$4;
+parsesPath=$5$1_$2;
+GSPath=$6;
 
 java -classpath $STREAMPATH/out/production/stream-parser:/home/andres/src/ojalgo-47.1.1.jar mstparser.RunParser $winObserve $winParse $calculateScores $exportScores $vocabFilePath $scoresFilePath $observeCorpusPath $corpusPath $parsesPath
 
