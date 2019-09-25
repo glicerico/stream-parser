cf7ab883768fc2cf341691d1e4ce5e7d03fe5c22
glicerico <suarezandres@gmail.com>
2019-09-25 22:58:03 +0800

    automating evaluation in stream_evaluate

--- src/scripts/stream_evaluate.sh
+++ src/scripts/stream_evaluate.sh
@@ -3,20 +3,44 @@
 # Runs stream parser on given corpus, and evaluates resulting parses against given GS using
 # github.com/singnet/language-learning parse-evaluator
 #
-# Usage:  stream_evaluate.sh <winObserve> <winParse> <vocabFilePath> <corpusPath> <parsesPath> <GSPath>
+# Usage:  stream_evaluate.sh <maxWinObserve> <maxWinParse> <vocabFilePath> <corpusPath> <parsesPath> <GSPath>
 
 STREAMPATH='/home/andres/IdeaProjects/stream-parser';
-winObserve=$1;
-winParse=$2;
+maxWinObserve=$5;
+maxWinParse=$6;
 calculateScores=true;
 exportScores=false;
-vocabFilePath=$3;
-scoresFilePath="/home/andres/Documents/CAiN/trash.scores";
-observeCorpusPath=$4;
-corpusPath=$4;
-parsesPath=$5$1_$2;
-GSPath=$6;
+vocabFilePath=$1;
+observeCorpusPath=$2;
+corpusPath=$2;
+GSPath=$4;
 
-java -classpath $STREAMPATH/out/production/stream-parser:/home/andres/src/ojalgo-47.1.1.jar mstparser.RunParser $winObserve $winParse $calculateScores $exportScores $vocabFilePath $scoresFilePath $observeCorpusPath $corpusPath $parsesPath
+echo "winObserve winParse Recall Precision F1" > results.dat # write header
 
-parse-evaluator -i -r $GSPath -t $parsesPath
+# Loop to parse and evaluate with range of windows
+
+for ((winObserve=1; winObserve<=maxWinObserve; winObserve++))
+do
+  for ((winParse=1; winParse<=maxWinObserve; winParse++))
+  do
+    parsesPath=$3${winObserve}_${winParse};
+    scoresFilePath=""; # Only used if exportScores is true
+
+    # Parse the corpus with current window sizes
+    java -classpath $STREAMPATH/out/production/stream-parser:/home/andres/src/ojalgo-47.1.1.jar mstparser.RunParser "$winObserve" $winParse $calculateScores $exportScores $vocabFilePath $scoresFilePath $observeCorpusPath $corpusPath $parsesPath
+
+    # Evaluate current parses against the gold standard
+    parse-evaluator -i -r $GSPath -t $parsesPath
+
+    # Obtain scores from evaluation
+    scores=() # scores array
+    cat ${parsesPath}.stat | grep -E 'Recall|Precision|F1' > tmp.file # tmp.file used to avoid subshell from piping
+    while read f1 f2
+    do
+      scores+=($f2)
+    done < tmp.file
+    rm tmp.file
+
+    echo "$winObserve" "$winParse" "${scores[@]}" >> results.dat
+  done
+done
